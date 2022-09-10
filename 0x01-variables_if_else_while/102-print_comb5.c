3aae7d7e0257763522bff3bf797737563c2f38e6
Ajala Sodiq Sunkanmi <sunkky17@gmail.com>
2022-09-10 10:39:51 -0700

    commit for the last task on variables in c-programm

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,41 +7,26 @@
  */
 int main(void)
 {
-	int i, e, g, h, op1, op2;
+	int i, p;
 
-	i = e = g = h = 48;
-	while (h < 58)
+	for (i = 0; i < 100; i++)
 	{
-		g = 48;
-		while (g < 58)
+		for (p = 0; p < 100; j++)
 		{
-			e = 48;
-			while (e < 58)
+			if (p > i)
 			{
-				i = 48;
-				while (i < 58)
+				putchar((i / 10) + '0' );
+				putchar((i % 10) + '0' );
+				putchar(' ');
+				putchar((p / 10) + '0');
+				putchar((p % 10) + '0');
+				if (i != 98)
 				{
-					op1 = (h * 10) + g;
-					op2 = (e * 10) + i;
-					if (op1 < op2)
-					{
-						putchar(h);
-						putchar(g);
-						putchar(' ');
-						putchar(e);
-						putchar(i);
-						if (h == 57 && g == 56 && e == 57 && i == 57)
-							break;
-						putchar(',');
-						putchar(' ');
-					}
-					i++;
+					putchar(',');
+					putchar(' ');
 				}
-				e++;
 			}
-			g++;
 		}
-		h++;
 	}
 	putchar('\n');
 	return (0);
