c683d6c8858ba5d4d792b0919448498da01a1780
Ajala Sodiq Sunkanmi <sunkky17@gmail.com>
2022-09-10 06:15:56 -0700

    This is commit for correction of task one in variables of c-program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	int n, 1;
+	int n, l;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -18,7 +18,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	else if (1 == 0)
+	else if (l == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
