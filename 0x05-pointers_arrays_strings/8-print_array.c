c51c092032e0723feb290bb988630b0ca85b5c02
Ajala Sodiq Sunkanmi <sunkky17@gmail.com>
2022-09-19 17:44:14 -0700

    This is commit for task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,7 +4,7 @@
 /**
  * print_array - prints n elements of an array of integers.
  * @a: input array.
- * @n: input n elements 
+ * @n: input n elements
  * Return: no return.
  */
 void print_array(int *a, int n)
